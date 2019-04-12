ae9280475e860fc22edaba618f5cca4c0ab69018
eshvk <807898+eshvk@users.noreply.github.com>
2019-04-12 14:23:30 -0700

    Reformatting code via [`black`](https://github.com/ambv/black).
    Steps to reproduce:
    - `pip install black`
    - `black <source code directory>`

--- ml-agents/setup.py
+++ ml-agents/setup.py
@@ -5,7 +5,7 @@ from io import open
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
@@ -13,21 +13,18 @@ setup(
     version='0.8.1',
     description='Unity Machine Learning Agents',
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/Unity-Technologies/ml-agents',
-    author='Unity Technologies',
-    author_email='ML-Agents@unity3d.com',
-
+    long_description_content_type="text/markdown",
+    url="https://github.com/Unity-Technologies/ml-agents",
+    author="Unity Technologies",
+    author_email="ML-Agents@unity3d.com",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.6'
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.6",
     ],
-
-    packages=['mlagents.trainers', 'mlagents.trainers.bc', 'mlagents.trainers.ppo'],  # Required
+    packages=["mlagents.trainers"],  # Required
     zip_safe=False,
-
     install_requires=[
         'mlagents_envs==0.8.1',
         'tensorflow>=1.7,<1.8',
@@ -43,10 +40,5 @@ setup(
         'pypiwin32==223;platform_system=="Windows"'],
 
     python_requires=">=3.6,<3.7",
-
-    entry_points={
-        'console_scripts': [
-            'mlagents-learn=mlagents.trainers.learn:main',
-        ],
-    },
+    entry_points={"console_scripts": ["mlagents-learn=mlagents.trainers.learn:main"]},
 )
